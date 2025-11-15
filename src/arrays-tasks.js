b8214178e611f56d8e1aa3d33c6514f9a1b377e5
vufimcev <buktor@bk.ru>
2025-11-14 23:12:05 -0800

    feat: complete getHeat

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -193,8 +193,18 @@ function isValueEqualsIndex(arr) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  const result = [];
+  for (let i = 0; i < arr.length + 1; i += 1) {
+    if (i < index) {
+      result[i] = arr[i];
+    } else if (i === index) {
+      result[i] = item;
+    } else {
+      result[i] = arr[i - 1];
+    }
+  }
+  return result;
 }
 
 /**
@@ -209,8 +219,12 @@ function insertItem(/* arr, item, index */) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  const result = [];
+  for (let i = 0; i < n && i < arr.length; i += 1) {
+    result[i] = arr[i];
+  }
+  return result;
 }
 
 /**
