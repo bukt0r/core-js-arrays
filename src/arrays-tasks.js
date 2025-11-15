ea7ba31920901f53ef01f35dcc6a429237708460
vufimcev <buktor@bk.ru>
2025-11-15 01:59:52 -0800

    feat: complete findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -660,8 +660,22 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  if (nums.length === 0) return 0;
+
+  let maxLen = 1;
+  let currentLen = 1;
+
+  for (let i = 1; i < nums.length; i += 1) {
+    if (nums[i] > nums[i - 1]) {
+      currentLen += 1;
+      if (currentLen > maxLen) maxLen = currentLen;
+    } else {
+      currentLen = 1;
+    }
+  }
+
+  return maxLen;
 }
 
 /**
